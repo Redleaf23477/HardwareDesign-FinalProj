d554d4eee872526c958c5b08eceb9816497a0db7
GuluLingpo <davidtaoyuan@gmail.com>
2018-12-20 19:09:33 +0800

    Asset OuO

---
+++ Asset/Scut Pixel Tileset - 8x8 Roguelike/PetesTiledExperiments.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="DungeonTilesetSprite" tilewidth="8" tileheight="8" tilecount="136" columns="8">
+ <image source="Graphic Revision Tileset 02.png" trans="898989" width="64" height="136"/>
+</tileset>
